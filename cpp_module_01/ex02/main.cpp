d00eb2012ebd411896733dbe91e78d025f54476f
Jozef Pluta <jpluta@c3r2s4.42prague.com>
2025-12-16 17:39:37 +0100

    changing cluster

--- cpp_module_01/ex02/main.cpp
+++ cpp_module_01/ex02/main.cpp
@@ -6,7 +6,26 @@
 /*   By: jpluta <jpluta@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/16 17:15:38 by jpluta            #+#    #+#             */
-/*   Updated: 2025/12/16 17:15:39 by jpluta           ###   ########.fr       */
+/*   Updated: 2025/12/16 17:34:29 by jpluta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <string>
+#include <iostream>
+
+int main() {
+	
+	std::string string = "HI THIS IS BRAIN";
+	std::string* string_ptr = &string;
+	std::string& string_ref = string; 
+	
+	std::cout << "memory address of the string		:" << &string << std::endl;
+	std::cout << "memory address held by stringPTR	:" << &string_ptr << std::endl;
+	std::cout << "memory address held by stringREF	:" << &string_ref << std::endl;
+	
+	std::cout << "value of the string variable		:" << string << std::endl;
+	std::cout << "value pointed to by stringPTR		:" << string_ptr << std::endl;
+	std::cout << "value pointed to by stringREF		:" << string_ref << std::endl;
+
+	return 0;
+}
