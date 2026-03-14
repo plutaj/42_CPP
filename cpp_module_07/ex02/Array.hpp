dc50922d85b5783f3edf616dac5955f3dde542a8
plutaj <jozefp048@gmail.com>
2026-03-14 17:57:01 +0100

    module 07 done

--- cpp_module_07/ex02/Array.hpp
+++ cpp_module_07/ex02/Array.hpp
@@ -1,6 +1,8 @@
 #ifndef ARRAY_HPP
 #define ARRAY_HPP
 
+#include <exception>
+
 template <typename T>
 class Array {
 
@@ -9,28 +11,42 @@ class Array {
         unsigned int _size;
 
     public:
-        Array() : _data(NULL), _size(0) {};
-        Array(unsigned int n) {
+        Array() : _data(nullptr), _size(0) {};
+        Array(unsigned int n) : _size(n) {
             _data = new T[n]();
         }
-        Array(const Array& obj) {
-            obj._data(this->_data);
-            obj._size(this->_size);
+        Array(const Array& obj) : _size(obj._size) {
+            _data = new T[_size];
+            for (unsigned int i = 0; i < _size; ++i)
+                _data[i] = obj._data[i];
         }
         Array& operator=(const Array& obj) {
-            if (*this != obj) {
-                this->_data = obj._data;
-                this->_size = obj._size;
+            if (this != &obj) {
+                delete[] _data;
+                _size = obj._size;
+                _data = new T[_size];
+                for (unsigned int i = 0; i < _size; ++i)
+                    _data[i] = obj._data[i];
             }
-            return (*this);
+            return *this;
+        }
+        T& operator[](unsigned int index) {
+            if (index >= _size)
+                throw IndexOutOfBounds();
+            return _data[index];
         }
         ~Array() {
             delete[] _data;
         }
-
         unsigned int size() {
             return _size;
         }
+        class IndexOutOfBounds : public std::exception {
+            public:
+                const char* what() const throw() {
+                    return "Index out of bounds";
+                }
+            };
 };
 
 #endif
--- cpp_module_07/ex02/main.cpp
+++ cpp_module_07/ex02/main.cpp
@@ -1,7 +1,44 @@
 #include "Array.hpp"
+#include <iostream>
+
+template <typename T>
+void printer(Array<T>& name) {
+    for (unsigned int i = 0; i < name.size(); i++) {
+        std::cout << name[i] << std::endl;
+    }
+}
 
 int main(void) {
+{
+    std::cout << "\nString array test" << std::endl;
 
+    Array<std::string> arr(3);
+    arr[0] = "This";
+    arr[1] = "is";
+    arr[2] = "test";
+    printer(arr);
+}
+{
+    std::cout << "\nInt array test" << std::endl;
 
+    Array<int> arr(4);
+    arr[0] = 9;
+    arr[1] = 2;
+    arr[2] = 5;
+    arr[3] = 1;
+    printer(arr);
+}
+{
+    std::cout << "\nEmpty array test" << std::endl;
+
+    Array<std::string> arr;
+
+    try {
+        std::cout << arr[0] << std::endl;
+    }
+    catch (const Array<std::string>::IndexOutOfBounds& e) {
+            std::cout << e.what() << std::endl;
+    }
+}
     return 0;
 }
